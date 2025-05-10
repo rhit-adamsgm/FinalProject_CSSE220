8756c577069667493b8829597b294df869d365fd
rhit-adamsgm <adamsgm@rose-hulman.edu>
2025-05-10 13:24:24 -0400

    This commit gives the rudimentary pieces of the start up screen when Game is first started, which should then go to the map screen.

--- src/cityRampage/Game.java
+++ src/cityRampage/Game.java
@@ -1,6 +1,11 @@
 package cityRampage;
 
 public class Game {
+	//fields----------------------------------------//
+		//fields for startup------------------------//
+	private static String username;
+	private static StartScreen startScreen;
+	//----------------------------------------------//
 
 	public static void main(String[] args) {
 		startUp();
@@ -10,7 +15,15 @@ public class Game {
 	 * This is basically a main, but callable from within the game. It will restart the game.
 	 */
 	public static void startUp() {
-		
+		startScreen = new StartScreen(() -> onStartScreenClose());
+	}
+	
+	/**
+	 * This pushes the code to the true start of the game after the startup screen is closed
+	 */
+	private static void onStartScreenClose() {
+		username = startScreen.getReturnedUsername();
+		System.out.println(username);//test code here
 	}
 
 }
--- src/cityRampage/StartScreen.java
+++ src/cityRampage/StartScreen.java
@@ -16,8 +16,13 @@ public class StartScreen extends JFrame {
 	private final JTextField input;
 	private final JPanel buttonPanel;
 	private final JButton startButton;
+	private String returnedUsername = "";
+	private boolean gameReady = false;
+	private Runnable onCloseCallback;
 	
-	public StartScreen() {
+	public StartScreen(Runnable onCloseCallback) {
+		this.onCloseCallback = onCloseCallback;
+
 		//enter username panel
 		textPanel = new JPanel();
 		label = new JLabel("Enter Your Emporer's Name");
@@ -27,11 +32,36 @@ public class StartScreen extends JFrame {
 		textPanel.add(label, BorderLayout.NORTH);
 		textPanel.add(input, BorderLayout.SOUTH);
 		
-		
-		
+		//start game panel
 		buttonPanel = new JPanel();
-		startButton = new JButton();
+		startButton = new JButton("Start City Rampage");
+		startButton.addActionListener(e -> startGame());
+		buttonPanel.add(startButton);
 		
+		//add the panels
+		add(textPanel, BorderLayout.NORTH);
+		add(buttonPanel, BorderLayout.SOUTH);
 		
+		setSize(600, 400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	private void startGame() {
+		if (!input.getText().trim().equals("")) {
+			returnedUsername = input.getText().trim();
+			gameReady = true;
+			dispose();
+			onCloseCallback.run();
+		}
+	}
+	
+	public boolean getGameReady() {
+		return gameReady;
+	}
+	
+	public String getReturnedUsername() {
+		return returnedUsername;
 	}
 }
